8447000e9aaaaef46d0b1ba4e38e17723380f018
Alex <alex_cui86@163.com>
2017-03-05 13:24:03 +0800

    Second commit before leaving with basic listview UI.

--- app/src/main/java/com/meee/remeber/remembermeee/MainActivity.java
+++ app/src/main/java/com/meee/remeber/remembermeee/MainActivity.java
@@ -2,12 +2,49 @@ package com.meee.remeber.remembermeee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    List<String> data;
+    private TimeLineAdapter timeLineAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setDividerHeight(0);
+        timeLineAdapter = new TimeLineAdapter(this, getData());
+        listView.setAdapter(timeLineAdapter);
+    }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "这是第1行测试数据");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "这是第2行测试数据");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "这是第3行测试数据");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "这是第4行测试数据");
+        list.add(map);
+
+        return list;
     }
 }
